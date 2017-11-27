566a5c80fb69deb0a536c0ad219285f7bd5fac03
gree-gorey <ignatyeff.g@yandex.ru>
2017-11-27 18:50:25 +0300

    add network agent

---
+++ agent/network.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os/exec"
+	"bytes"
+	"log"
+)
+
+
+func connections() string {
+	netstatCmd := "netstat -tn 2>/dev/null | tail -n +3 | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head"
+	cmd := exec.Command(
+		"/bin/bash",
+		"-c",
+		netstatCmd,
+	)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return out.String()
+}
+
