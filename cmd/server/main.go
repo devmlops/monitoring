c1d38f081a911cd3ccde14cab73f51d1abef17d3
Alexander Kirillov <thomson77777@mail.ru>
2017-12-07 00:29:23 +0300

    typo in func name RunTelegramBot

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	config := OpenConfig("config.json")
-	bot := RunTegelegramBot(config.TelegramBot.Token)
+	bot := RunTelegramBot(config.TelegramBot.Token)
 	//route := HttpServer(&Store{users: config.TelegramBot.Users, bot: bot})
 	SendAlert(bot, config.TelegramBot.Users, "It's Ok" )
 	monitor := Monitor{bot: bot, config: config}
