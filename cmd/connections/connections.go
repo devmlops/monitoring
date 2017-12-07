d3c2eacaa1732cf9733c427d4fb26526021e9852
gree-gorey <ignatyeff.g@yandex.ru>
2017-12-07 16:16:36 +0300

    add stress test connections

---
+++ cmd/connections/connections.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "net"
+    "fmt"
+    "time"
+    "sync"
+)
+
+func main()  {
+    var wg sync.WaitGroup
+    wg.Add(100)
+    for i := 0; i <= 100; i++ {
+        go func() {
+            defer wg.Done()
+            _, err := net.Dial("tcp", "127.0.0.1:8080")
+            time.Sleep(10 * time.Minute)
+            if err != nil {
+                fmt.Println("foo")
+            }
+        }()
+    }
+    wg.Wait()
+}
